5c1de94dfca77fe4a42e633a5d564ea62d685986
Sebastian Velador Rocha <jenaro>
2024-05-16 18:06:36 -0600

    RecyclerView

---
+++ app/src/main/java/com/example/recyclerviewkotlin/CustomAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.recyclerviewkotlin
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class CustomAdapter: RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+
+    val titles = arrayOf("Velador",
+        "suscribete",
+        "videos",
+        "youtube")
+
+    val details = arrayOf("Muchos videos Nuevos",
+        "kotlin",
+        "+ Videos ",
+        "Extras")
+
+    val images = intArrayOf(R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground)
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
+        val v = LayoutInflater.from(viewGroup.context).inflate(R.layout.card_layout, viewGroup,false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
+        viewHolder.itemTitle.text = titles[i]
+        viewHolder.itemDetail.text = details[i]
+        viewHolder.itemImage.setImageResource(images[i])
+    }
+
+    override fun getItemCount(): Int {
+        return titles.size
+    }
+
+    inner class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+        var itemImage: ImageView
+        var itemTitle: TextView
+        var itemDetail: TextView
+
+        init {
+            itemImage = itemView.findViewById(R.id.item_image)
+            itemTitle = itemView.findViewById(R.id.item_title)
+            itemDetail = itemView.findViewById(R.id.item_detal)
+        }
+    }
+}
